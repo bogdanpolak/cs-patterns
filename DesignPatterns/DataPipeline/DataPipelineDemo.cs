5325444e005e8b5634fbf833df9a323b6b0c4ccb
Bogdan Polak <bogdanpol@gmail.com>
2021-06-08 23:37:07 +0200

    Initialize Data Pipeline Demo

---
+++ DesignPatterns/DataPipeline/DataPipelineDemo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DesignPatterns.DataPipeline
+{
+    public static class DataPipelineDemo
+    {
+        public static void RunDemo()
+        {
+            // TODO: get code from SO
+            // https://stackoverflow.com/questions/8308155/generic-pipeline-where-the-input-output-types-are-different-for-each-filter
+            Console.WriteLine("*** Under construction ***");
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -16,6 +16,9 @@ namespace DesignPatterns
             Console.WriteLine("-----------------------------------------------".Pastel(Color.DarkGreen));
             Console.WriteLine("Chain Of Responsibility Pattern (behavioral)".Pastel(Color.Gold));
             ChainResponsibility.ChainOfResponsibilityDemo.RunDemo();
+            Console.WriteLine("-----------------------------------------------".Pastel(Color.DarkGreen));
+            Console.WriteLine("Data Pipeline Pattern (behavioral)".Pastel(Color.Gold));
+            DataPipeline.DataPipelineDemo.RunDemo();
         }
     }
 }
